0ea4870d369c4ef2695c85b3ec0bcbce6022593c
Adi Septriansyah <140364891+Protoflicker@users.noreply.github.com>
2025-10-30 06:53:26 +0000

    importing leaflet icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+
 import 'leaflet/dist/leaflet.css';
+
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
